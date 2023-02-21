6e88d57e87555c384283998b43c7c769099a6271
tendaikon <118843518+tendaikon@users.noreply.github.com>
2023-02-21 04:12:34 +0100

    opt: replace bsp recursion with loop

--- code/qcommon/cm_trace.c
+++ code/qcommon/cm_trace.c
@@ -1041,99 +1041,106 @@ void CM_TraceThroughTree( traceWork_t *tw, int num, float p1f, float p2f, vec3_t
 	vec3_t		mid;
 	int			side;
 	float		midf;
+	vec3_t		_p1;
 
 	if (tw->trace.fraction <= p1f) {
 		return;		// already hit something nearer
 	}
 
-	// if < 0, we are in a leaf node
-	if (num < 0) {
-		CM_TraceThroughLeaf( tw, &cm.leafs[-1-num] );
-		return;
-	}
-
-	//
-	// find the point distances to the separating plane
-	// and the offset for the size of the box
-	//
-	node = cm.nodes + num;
-	plane = node->plane;
-
-	// adjust the plane distance appropriately for mins/maxs
-	if ( plane->type < 3 ) {
-		t1 = p1[plane->type] - plane->dist;
-		t2 = p2[plane->type] - plane->dist;
-		offset = tw->extents[plane->type];
-	} else {
-		t1 = DotProduct (plane->normal, p1) - plane->dist;
-		t2 = DotProduct (plane->normal, p2) - plane->dist;
-		if ( tw->isPoint ) {
-			offset = 0;
-		} else {
-			// this is silly
-			offset = 2048;
+	// arg p1 points to the caller's stack; make it local so we can loop
+	VectorCopy(p1, _p1);
+	p1 = _p1;
+	while (1) {
+		// if < 0, we are in a leaf node
+		if (num < 0) {
+			CM_TraceThroughLeaf( tw, &cm.leafs[-1-num] );
+			return;
 		}
+
+		//
+		// find the point distances to the separating plane
+		// and the offset for the size of the box
+		//
+		node = cm.nodes + num;
+		plane = node->plane;
+
+		// adjust the plane distance appropriately for mins/maxs
+		if ( plane->type < 3 ) {
+			t1 = p1[plane->type] - plane->dist;
+			t2 = p2[plane->type] - plane->dist;
+			offset = tw->extents[plane->type];
+		} else {
+			t1 = DotProduct (plane->normal, p1) - plane->dist;
+			t2 = DotProduct (plane->normal, p2) - plane->dist;
+			if ( tw->isPoint ) {
+				offset = 0;
+			} else {
+				// this is silly
+				offset = 2048;
+			}
+		}
+
+		// see which sides we need to consider
+		if ( t1 >= offset + 1 && t2 >= offset + 1 ) {
+			num = node->children[0];
+			continue;
+		}
+		if ( t1 < -offset - 1 && t2 < -offset - 1 ) {
+			num = node->children[1];
+			continue;
+		}
+
+		// put the crosspoint SURFACE_CLIP_EPSILON pixels on the near side
+		if ( t1 < t2 ) {
+			idist = 1.0/(t1-t2);
+			side = 1;
+			frac2 = (t1 + offset + SURFACE_CLIP_EPSILON)*idist;
+			frac = (t1 - offset + SURFACE_CLIP_EPSILON)*idist;
+		} else if (t1 > t2) {
+			idist = 1.0/(t1-t2);
+			side = 0;
+			frac2 = (t1 - offset - SURFACE_CLIP_EPSILON)*idist;
+			frac = (t1 + offset + SURFACE_CLIP_EPSILON)*idist;
+		} else {
+			side = 0;
+			frac = 1;
+			frac2 = 0;
+		}
+
+		// move up to the node
+		if ( frac < 0 ) {
+			frac = 0;
+		}
+		if ( frac > 1 ) {
+			frac = 1;
+		}
+
+		midf = p1f + (p2f - p1f)*frac;
+
+		mid[0] = p1[0] + frac*(p2[0] - p1[0]);
+		mid[1] = p1[1] + frac*(p2[1] - p1[1]);
+		mid[2] = p1[2] + frac*(p2[2] - p1[2]);
+
+		CM_TraceThroughTree( tw, node->children[side], p1f, midf, p1, mid );
+
+		// go past the node
+		if ( frac2 < 0 ) {
+			frac2 = 0;
+		}
+		if ( frac2 > 1 ) {
+			frac2 = 1;
+		}
+
+		midf = p1f + (p2f - p1f)*frac2;
+
+		mid[0] = p1[0] + frac2*(p2[0] - p1[0]);
+		mid[1] = p1[1] + frac2*(p2[1] - p1[1]);
+		mid[2] = p1[2] + frac2*(p2[2] - p1[2]);
+
+		num = node->children[side^1];
+		p1f = midf;
+		VectorCopy(mid, p1);
 	}
-
-	// see which sides we need to consider
-	if ( t1 >= offset + 1 && t2 >= offset + 1 ) {
-		CM_TraceThroughTree( tw, node->children[0], p1f, p2f, p1, p2 );
-		return;
-	}
-	if ( t1 < -offset - 1 && t2 < -offset - 1 ) {
-		CM_TraceThroughTree( tw, node->children[1], p1f, p2f, p1, p2 );
-		return;
-	}
-
-	// put the crosspoint SURFACE_CLIP_EPSILON pixels on the near side
-	if ( t1 < t2 ) {
-		idist = 1.0/(t1-t2);
-		side = 1;
-		frac2 = (t1 + offset + SURFACE_CLIP_EPSILON)*idist;
-		frac = (t1 - offset + SURFACE_CLIP_EPSILON)*idist;
-	} else if (t1 > t2) {
-		idist = 1.0/(t1-t2);
-		side = 0;
-		frac2 = (t1 - offset - SURFACE_CLIP_EPSILON)*idist;
-		frac = (t1 + offset + SURFACE_CLIP_EPSILON)*idist;
-	} else {
-		side = 0;
-		frac = 1;
-		frac2 = 0;
-	}
-
-	// move up to the node
-	if ( frac < 0 ) {
-		frac = 0;
-	}
-	if ( frac > 1 ) {
-		frac = 1;
-	}
-		
-	midf = p1f + (p2f - p1f)*frac;
-
-	mid[0] = p1[0] + frac*(p2[0] - p1[0]);
-	mid[1] = p1[1] + frac*(p2[1] - p1[1]);
-	mid[2] = p1[2] + frac*(p2[2] - p1[2]);
-
-	CM_TraceThroughTree( tw, node->children[side], p1f, midf, p1, mid );
-
-
-	// go past the node
-	if ( frac2 < 0 ) {
-		frac2 = 0;
-	}
-	if ( frac2 > 1 ) {
-		frac2 = 1;
-	}
-		
-	midf = p1f + (p2f - p1f)*frac2;
-
-	mid[0] = p1[0] + frac2*(p2[0] - p1[0]);
-	mid[1] = p1[1] + frac2*(p2[1] - p1[1]);
-	mid[2] = p1[2] + frac2*(p2[2] - p1[2]);
-
-	CM_TraceThroughTree( tw, node->children[side^1], midf, p2f, mid, p2 );
 }
 
 
